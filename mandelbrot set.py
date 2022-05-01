745f4f50da75d02d7aba3938794bd1556e9491a9
DreamStudios <84223012+Dream628@users.noreply.github.com>
2022-05-01 09:35:14 +0800

    Update mandelbrot set.py

--- mandelbrot set.py
+++ mandelbrot set.py
@@ -5,7 +5,7 @@ size = int(input("Size: "))
 image = Image.new("L", (size, size), (255))
 for x in range(size):
     for y in range(size):
-        z = 0
+        z = complex(0)
         c = (x - size/2 + (size/2 - y) * 1j)/(size/4)
         for n in range(255):
             if math.sqrt(z.real**2+z.imag**2) > 2:
